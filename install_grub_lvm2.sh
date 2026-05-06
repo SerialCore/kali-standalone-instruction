43c090c18f7f6d9c137c7ed0bad5ee9f66432248
SerialCore <serialcore@outlook.com>
2026-05-06 23:14:53 +0800

    update grub rescue

--- install_grub_lvm2.sh
+++ install_grub_lvm2.sh
@@ -8,13 +8,15 @@ apt-get install lvm2
 vgscan
 vgchange -ay
 lvscan
+# decrypt lvm
+cryptsetup luksOpen /dev/...drive... kali-root
 # mount lvm
 cd /
 mkdir /target
-mount /dev/...vg... /target
+mount /dev/mapper/kali--vg-root /target
 # install grub
 for n in dev dev/pts proc sys sys/firmware/efi/efivars run etc/resolv.conf; do mount --bind /$n /target/$n; done
 chroot /target
 mount -a
-grub-install --removable /dev/...drive...
+grub-install --removable /dev/{drive_name}
 update-grub
